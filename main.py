2151447017911813ad7469560a45ca3714ee9d07
islameehassan <islamee@aucegypt.edu>
2023-03-02 22:52:12 +0200

    solving a corner case: italain and ecuadorian leagues having the same exact league name

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def parseRequest():
     parse.add_argument("date", type=str, nargs = "?", help="Date formated as \"YY-MM-DD\" or \"today\" or \"yesterday\" or \"tomorrow\", in case of no date provided, today's date will be used")
     parse.add_argument("-l", "--league", default=leagues, nargs="*", choices = leagues, help="a league to display its results, if no league is provided, all the leagues will be displayed")
     parse.add_argument('-c', "--country", default=[], nargs="*", choices=countries, help="a country to display the results of all its leagues and cups (this will overwrite the league)")
-    parse.add_argument("--fbd", default=0, type=int, help="number of days to go frowards or backwards from the date provided, such that negative numbers will go backwards")
+    parse.add_argument("--fbd", default=0, type=int, help="number of days to go forwards or backwards from the date provided, such that negative numbers will go backwards")
     parse.add_argument('-a', "--all", default = False, action="store_true", help="display all the results of all leagues and cups (this will overwrite the league and country)")
     args = parse.parse_args()
     return args
